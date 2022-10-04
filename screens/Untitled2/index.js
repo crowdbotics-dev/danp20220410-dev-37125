bc447308cb8fe5fa2aec44a76b2ef64d81dbed53
Daniel Palacios <daniel80380@gmail.com>
2022-10-04 17:27:21 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,9 +1,12 @@
+import { Pressable } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-const Untitled2 = () => {
+const Untitled2 = ({
+  navigation
+}) => {
   return <View style={_styles.AutbMUwB}>
-      </View>;
+      <Pressable onPress={() => navigation.navigate("Untitled3")} style={_styles.sbstzdUD}><View style={_styles.WOqoLjlA}></View></Pressable></View>;
 };
 
 export default Untitled2;
@@ -14,5 +17,18 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  WOqoLjlA: {
+    left: 73,
+    top: 122,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  sbstzdUD: {
+    position: "unset"
   }
 });
