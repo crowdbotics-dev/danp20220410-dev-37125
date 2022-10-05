d7cdc57c8d42d56fe2520ae072c92082103fde39
Daniel Palacios <daniel80380@gmail.com>
2022-10-04 22:47:30 -0300

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled5/index.js
@@ -0,0 +1,32 @@
+import { Pressable } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled5 = () => {
+  return <View style={_styles.VraQOJVN}>
+      <Pressable onPress={() => navigation.navigate("Untitled6")} style={_styles.currbbkp}><View style={_styles.JEkValGm}></View></Pressable></View>;
+};
+
+export default Untitled5;
+
+const _styles = StyleSheet.create({
+  VraQOJVN: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  JEkValGm: {
+    left: 35,
+    top: 163,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  currbbkp: {
+    position: "unset"
+  }
+});
---
+++ screens/Untitled6/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled6 = () => {
+  return <View style={_styles.NWnPBNJx}>
+      </View>;
+};
+
+export default Untitled6;
+
+const _styles = StyleSheet.create({
+  NWnPBNJx: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
